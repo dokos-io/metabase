615033c139532b821f6824b6a2bba98a87c864e0
Charles-Henri Decultot <chdecultot@dokos.io>
2022-11-18 18:03:52 +0100

    wip: poc

--- metabase_integration/hooks.py
+++ metabase_integration/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/metabase_integration/css/metabase_integration.css"
-# app_include_js = "/assets/metabase_integration/js/metabase_integration.js"
+app_include_js = "metabase.bundle.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/metabase_integration/css/metabase_integration.css"
---
+++ metabase_integration/public/metabase.bundle.js
@@ -0,0 +1,161 @@
+import EditorJS from "@editorjs/editorjs";
+import Block from "frappe/public/js/frappe/views/workspace/blocks/block.js";
+
+export default class Metabase extends Block {
+	static get toolbox() {
+		return {
+			title: __("Metabase"),
+			icon: frappe.utils.icon("dashboard-list", "sm"),
+		};
+	}
+
+	static get isReadOnlySupported() {
+		return true;
+	}
+
+	constructor({ data, api, config, readOnly, block }) {
+		super({ data, api, config, readOnly, block });
+		this.col = this.data.col ? this.data.col : "12";
+        readOnly = 0;
+		this.allow_customization = !this.readOnly;
+		this.options = {
+			allow_sorting: this.allow_customization,
+			allow_create: this.allow_customization,
+			allow_delete: this.allow_customization,
+			allow_hiding: false,
+			allow_edit: true,
+			allow_resize: true,
+			min_width: 6,
+			max_widget_count: 2,
+		};
+        this.iFrameUrl = "";
+	}
+
+	render() {
+		this.wrapper = document.createElement("div");
+		this.iFrameUrl = `
+            <script id="resizer" src="http://localhost:3000/app/iframeResizer.js"></script>
+            <iframe
+                src="http://localhost:3000/embed/dashboard/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjF9LCJwYXJhbXMiOnt9LCJleHAiOjE2Njg3NjkxODF9.N8TXahc_JFp1Et_Z5YWwU0F8u0ovne1uUrmTPTEhYi4#bordered=false&titled=false"
+                frameborder="0"
+                width=100%
+                onload="iFrameResize({}, this)"
+                allowtransparency>
+            </iframe>`
+
+        $(this.iFrameUrl).appendTo(this.wrapper);
+		return this.wrapper;
+	}
+
+	validate(savedData) {
+        console.log(savedData)
+		if (!savedData.chart_name) {
+			return false;
+		}
+
+		return true;
+	}
+
+	save() {
+        console.log("SAVE")
+		// return {
+		// 	chart_name: this.wrapper.getAttribute("chart_name"),
+		// 	col: this.get_col(),
+		// 	new: this.new_block_widget,
+		// };
+        return {}
+	}
+}
+
+frappe.provide("frappe.views")
+
+frappe.views.Workspace.prototype.initialize_editorjs = function(blocks) {
+		this.tools = {
+			header: {
+				class: this.blocks["header"],
+				inlineToolbar: ["HeaderSize", "bold", "italic", "link"],
+				config: {
+					default_size: 4,
+				},
+			},
+			paragraph: {
+				class: this.blocks["paragraph"],
+				inlineToolbar: ["HeaderSize", "bold", "italic", "link"],
+				config: {
+					placeholder: __("Choose a block or continue typing"),
+				},
+			},
+			chart: {
+				class: this.blocks["chart"],
+				config: {
+					page_data: this.page_data || [],
+				},
+			},
+            
+			card: {
+				class: this.blocks["card"],
+				config: {
+					page_data: this.page_data || [],
+				},
+			},
+			shortcut: {
+				class: this.blocks["shortcut"],
+				config: {
+					page_data: this.page_data || [],
+				},
+			},
+			onboarding: {
+				class: this.blocks["onboarding"],
+				config: {
+					page_data: this.page_data || [],
+				},
+			},
+			quick_list: {
+				class: this.blocks["quick_list"],
+				config: {
+					page_data: this.page_data || [],
+				},
+			},
+			spacer: this.blocks["spacer"],
+			HeaderSize: frappe.workspace_block.tunes["header_size"],
+            metabase: {
+				class: this.blocks["metabase"],
+				config: {
+                    default_size: 12,
+					page_data: this.page_data || [],
+				},
+			},
+		};
+		this.editor = new EditorJS({
+			data: {
+				blocks: blocks || [],
+			},
+			tools: this.tools,
+			autofocus: false,
+			readOnly: true,
+			logLevel: "ERROR",
+		});
+}
+
+const _get_original_actions_menu_items = frappe.views.ListView.prototype.get_actions_menu_items;
+
+const print_selected_items = () => {
+    frappe.msgprint("OK");
+}
+
+frappe.views.ListView.prototype.get_actions_menu_items = function() {
+    const actions_menu_items = _get_original_actions_menu_items.call(this)
+    actions_menu_items.forEach(f => {
+        if (f.label == __("Print")) {
+            f.action = print_selected_items
+        }
+    })
+    return actions_menu_items
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    Object.assign(frappe.workspace_block.blocks, {
+        metabase: Metabase,
+    })
+})
